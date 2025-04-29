402196eab3cea60eaa2121e7176b377045c704f3
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-04-29 10:53:18 +0700

    .

---
+++ ecommerce-nextjs-app/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+// import AuthLayout from "../components/layout"
+import { SigninForm } from "@/components/auth/sign-in/signin-form"
+
+export default function SigninPage() {
+  return (
+      <SigninForm />
+  )
+}
---
+++ ecommerce-nextjs-app/src/components/auth/sign-in/useSignin.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { z } from 'zod'
+import { LoginSchema } from '../schema/index'
+
+export function useSignin() {
+  const [loading, setLoading] = useState(false)
+  const router = useRouter()
+
+  const onSubmit = async (data: z.infer<typeof LoginSchema>) => {
+    setLoading(true)
+    console.log(data)
+    // Simulate successful login
+    setTimeout(() => {
+      setLoading(false)
+      router.push('/admin/dashboard') // Redirect after login
+    }, 2000)
+  }
+
+  return { onSubmit, loading }
+}
