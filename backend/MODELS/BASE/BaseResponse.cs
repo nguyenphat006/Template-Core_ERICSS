87b65c877b6d0ea477200a00a6f4ac0d20061608
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-10-05 00:08:22 +0700

    Add base model, request, and response classes
    Introduced ModelBase, BaseRequest, and BaseResponse classes in MODELS/BASE to standardize model structure and API responses. Also added an empty MappingProfile class for AutoMapper setup and updated project files to reflect new folders and files.

---
+++ backend/MODELS/BASE/BaseResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace MODELS.BASE
+{
+    public class BaseResponse<T>
+    {
+        public bool Success { get; set; }
+        required
+        public string Message { get; set; } 
+        public T? Data { get; set; }
+
+        public static BaseResponse<T> SuccessResponse(T data, string message = "") =>
+            new() { Success = true, Message = message, Data = data };
+
+        public static BaseResponse<T> FailResponse(string message) =>
+            new() { Success = false, Message = message, Data = default };
+
+    }
+}
---
+++ backend/MODELS/BASE/ModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELS.BASE
+{
+    public class ModelBase
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public string DeletedBy { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+    }
+}
